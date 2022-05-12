abef40bf85125d03f137402bb7fa7e238bf8a415
arnav-snowleo <prakashsinghaarav@gmail.com>
2022-05-12 12:16:36 +0530

    postgres db connected

--- firstDjangoAPI/settings.py
+++ firstDjangoAPI/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'firstDjangoAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'employee_crud_db',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres', 
+        'HOST' : 'localhost',
     }
 }
 
