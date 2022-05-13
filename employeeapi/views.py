e0b817ad15367bebe5369053ad3224855a9d774a
arnav-snowleo <prakashsinghaarav@gmail.com>
2022-05-13 13:21:15 +0530

    view added in views.py

--- employeeapi/views.py
+++ employeeapi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def current_datetime_view(request):
+    now = datetime.datetime.now()
+    html = "<html><body>The time now is %s.</body></html>" % now
+    return HttpResponse(html)
--- firstDjangoAPI/urls.py
+++ firstDjangoAPI/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path , include
 
+from employeeapi.views import current_datetime_view
+
 from .routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , current_datetime_view),
     path('api/', include(router.urls)),  # 'api/' is the base path
 ]
